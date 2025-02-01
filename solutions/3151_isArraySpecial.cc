ee97e42090a5d3258fff30d98afab3d1cd5ad2e3
Perry Chien <peiruchien1@gmail.com>
2025-01-31 18:14:38 -0800

    1/31

---
+++ solutions/3151_isArraySpecial.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        bool mask = 1 & nums[0];
+        bool next;
+        for (int i = 1; i < nums.size(); ++i) {
+            next = 1 & nums[i];
+            if (mask == next) return false;
+            mask = next;
+        }
+
+        return true;
+    }
+};

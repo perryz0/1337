6c835a656afdb0e397093fcbb5d605355fe34376
Perry Chien <peiruchien1@gmail.com>
2025-02-26 00:27:09 -0800

    2/26

---
+++ solutions/1749_maxAbsoluteSum.cc
@@ -0,0 +1,18 @@
+// Feb. 26th, 2025 (daily)
+
+class Solution {
+    public:
+        int maxAbsoluteSum(vector<int>& nums) {
+            int mx = nums[0], mn = nums[0];
+            int n = nums.size();
+            int max_curr = mx, min_curr = mn;
+    
+            for (int i = 1; i<n; ++i) {
+                max_curr = max(nums[i], max_curr+nums[i]);
+                min_curr = min(nums[i], min_curr+nums[i]);
+                mx = max(mx, max_curr); mn = min(mn, min_curr);
+            }
+    
+            return max(mx, abs(mn));
+        }
+    };

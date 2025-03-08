c0a3282c8896966c5f632c07e1fe7d12f93d722d
Perry Chien <peiruchien1@gmail.com>
2025-03-08 12:28:18 -0800

    3/7

---
+++ solutions/2379_minimumRecolors.cc
@@ -0,0 +1,25 @@
+// Mar. 8th, 2025 (daily)
+
+class Solution {
+    public:
+        int minimumRecolors(string blocks, int k) {
+            int wcnt = 0;
+            int minChanges = k;
+    
+            // init window
+            for (int i = 0; i<k; ++i) {
+                if (blocks[i] == 'W') wcnt++;
+            }
+    
+            minChanges = wcnt;
+    
+            for (int i = k; i<blocks.size(); ++i) {
+                if (blocks[i] == 'W') wcnt++;
+                if (blocks[i-k] == 'W') wcnt--;
+    
+                minChanges = min(minChanges, wcnt);
+            }
+    
+            return minChanges;
+        }
+    };

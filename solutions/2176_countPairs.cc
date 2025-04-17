f210800661ba77418a780ea755f90719ab8a7143
Perry Chien <peiruchien1@gmail.com>
2025-04-16 18:56:40 -0700

    4/16

---
+++ solutions/2176_countPairs.cc
@@ -0,0 +1,16 @@
+// Apr. 16th, 2025 (daily)
+
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        int n = nums.size();
+        int cnt = 0;
+        for (int i = 0; i<n; ++i) {
+            for (int j = i+1; j<n; ++j) {
+                if (nums[i] == nums[j] && (i*j)%k == 0) cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
